d4932ae2f70776b97d99aa8e443ba69cddc37bb7
Katarzyna <kaj915@wp.pl>
2015-09-18 00:27:57 +0200

    test1ref

--- src/RomanNumeralsTest.java
+++ src/RomanNumeralsTest.java
@@ -1,14 +1,21 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class RomanNumeralsTest {
 
+	private RomanNumerals arabicNumber;
+	
+	@Before
+	public void setUp() throws Exception {
+		arabicNumber = new RomanNumerals();
+	}
 	@Test
 	public void testConverts0ToABlankString() {
-		RomanNumerals number = new RomanNumerals();
-		String roman = number.convert(0);
+		
+		String roman = arabicNumber.convert(0);
 		assertEquals(roman, " ");
 	}
 
