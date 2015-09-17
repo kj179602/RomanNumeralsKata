11a2f94cd69da3ecef8a505cb19ebb2d07b47475
Katarzyna <kaj915@wp.pl>
2015-09-18 01:01:32 +0200

    test4green

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,12 +6,19 @@ public class RomanNumerals {
 		if(number == 0){
 			return " ";
 		}
-		else if(number == 1){
-			return "I";
+		else if(number == 1 || number == 2){
+			
+			return multiplyNumber(number, "I");
 		}
 		else{
 			return "V";
 		}
 	}
-
+	public String multiplyNumber(int number, String romanNumber){
+		String newNumber = "";
+		for(int i = 0; i< number; i++){
+			newNumber = romanNumber + newNumber;
+		}
+		return newNumber;
+	}
 }
