84e6e502aead71050eec08a3c92f24d74d9fe69e
Katarzyna <kaj915@wp.pl>
2015-09-18 02:47:39 +0200

    test9ref

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,30 +1,37 @@
 
 public class RomanNumerals {
 
+	private static String ROMAN_I = "I";
+	private static String ROMAN_V = "V";
+	private static String ROMAN_X = "X";
+	private static String ROMAN_C = "C";
 	public String convert(int arabicNumber) {
 		
 		switch(arabicNumber){
 		
 		case 1:
 		case 2:
-			return multiplyNumber(arabicNumber, "I");
+			return multiplyNumber(arabicNumber, ROMAN_I);
 		case 5:
-			return "V";
+			return ROMAN_V;
 		case 6:
 		case 7:
 			arabicNumber -= 5;
-			return "V" + multiplyNumber(arabicNumber, "I");
+			return ROMAN_V + multiplyNumber(arabicNumber, ROMAN_I);
 		case 20:
 			arabicNumber /= 10;
-			return multiplyNumber(arabicNumber, "X");
+			return multiplyNumber(arabicNumber, ROMAN_X);
 		case 23:
-			return multiplyNumber(2, "X") + multiplyNumber(3, "I");
+			
+			return multiplyNumber(2, ROMAN_X) + multiplyNumber(3, ROMAN_I);
 		case 300:
 			arabicNumber /= 100;
-			return multiplyNumber(arabicNumber, "C");
+			return multiplyNumber(arabicNumber, ROMAN_C);
 		default:
 			return " ";
 		}
+	
+	}public void convertOther(int number){
 		
 	}
 	public String multiplyNumber(int arabicNumber, String romanNumber){
